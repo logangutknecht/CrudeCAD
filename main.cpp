8ad4fbb082205022d72a6a096271d74cd9ce4b5b
Logan Gutknecht <logan.gutknecht@convergent-mfg.com>
2023-11-10 22:25:08 -0500

    depricated the use of QT for ease of application

--- main.cpp
+++ main.cpp
@@ -1,11 +1,69 @@
-#include <QApplication>
-#include "Controller.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-int main(int argx, char *argv[]) {
-    QApplication a(argc, argv);
+// Model
+class StepModel {
+public:
+    void loadStepFile(const std::string& filePath) {
+        std::ifstream file(filePath);
+        if (file.is_open()) {
+            std::string line;
+            while (std::getline(file, line)) {
+                stepData.push_back(line);
+            }
+            file.close();
+        } else {
+            std::cerr << "Unable to open file: " << filePath << std::endl;
+        }
+    }
 
-    Controller controller;
-    controller.showMainWindow();
+    const std::vector<std::string>& getStepData() const {
+        return stepData;
+    }
 
-    return a.exec();
+private:
+    std::vector<std::string> stepData;
+};
+
+// View
+class StepView {
+public:
+    void displayMessage(const std::string& message) const {
+        std::cout << message << std::endl;
+    }
+
+    void displayStepData(const std::vector<std::string>& stepData) const {
+        for (const auto& line : stepData) {
+            std::cout << line << std::endl;
+        }
+    }
+};
+
+// Controller
+class StepController {
+public:
+    void processStepFile(const std::string& filePath) {
+        model.loadStepFile(filePath);
+        view.displayMessage("Step file loaded successfully.");
+        view.displayStepData(model.getStepData());
+        // Add further logic for manipulation or visualization here
+    }
+
+private:
+    StepModel model;
+    StepView view;
+};
+
+int main() {
+    std::string filePath;
+    std::cout << "Enter the path to the STEP file: ";
+    std::getline(std::cin, filePath);
+
+    StepController controller;
+    controller.processStepFile(filePath);
+
+    return 0;
 }
